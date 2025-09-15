b45636a7334a8a0e726fa020c5f0a0758fcc3080
devansh joshi <devansh0814dj@gmail.com>
2025-09-15 15:57:13 +0530

    15sep

--- controllers/incidentCntrl.js
+++ controllers/incidentCntrl.js
@@ -4,16 +4,7 @@ const { User } = require('../models/userModel')
 
 const fs = require('fs');
 const path = require('path')
-const AWS = require('aws-sdk')
 require('dotenv').config();
-
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'us-east-2'
-})
-
-const s3 = new AWS.S3();
 
 const addIncident = asyncHandler(async (req, res) => {
 
@@ -21,34 +12,7 @@ const addIncident = asyncHandler(async (req, res) => {
     const note= req.file.path
 
     
-    if(note){
-        const params = {
-            Bucket: process.env.AWS_BUCKET_NAME,
-            Key: note,
-            Body: fs.createReadStream(req.file.path),
-            ContentType: mimeType
-        }
-        const s3Response = await s3.upload(params).promise();
-        const incFile = s3Response.Location;
-       
-
-        const incident = await Incident.create({
-            user,
-            report,
-            pincodeOfIncident,
-            address,
-            meidaSt: incFile
-        })
-
-
-
-        if(incident){
-            res.status(201).json({message: "Incident reported successfully"})
-        }else{
-            res.status(500).json({message: "Something went wrong"})
-        }
-    }else{
-        const incident = await Incident.create({
+    const incident = await Incident.create({
             user,
             report,
             address,
@@ -60,7 +24,6 @@ const addIncident = asyncHandler(async (req, res) => {
         }else{
             res.status(500).json({message: "Something went wrong"})
         }
-    }
 
 });
 
