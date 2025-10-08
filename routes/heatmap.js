7f88856e48c493f735dbd179e9195ace0312a2b4
devansh joshi <devansh0814dj@gmail.com>
2025-10-08 18:28:10 +0530

    update

--- routes/heatmap.js
+++ routes/heatmap.js
@@ -5,7 +5,7 @@ const { Emergency } = require("../models/emergencyModel");
 
 router.route("/").post(async (req, res) => {
   if (req.userId) res.status(401).send({ error: "no userId" });
-  if (req.lat) res.status(401).send({ error: "no lat" });
+  if (req.lat) res.status(401).send({ error: "no lat" ,req});
   if (req.long) res.status(401).send({ error: "no long" });
   // start processing
   const {pincode} = await getData(
